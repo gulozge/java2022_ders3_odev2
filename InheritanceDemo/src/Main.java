0acc2e777f8697fcf50ae3843fdb4df25c6116fc
gulozge <110093011+gulozge@users.noreply.github.com>
2022-10-25 20:14:18 +0300

    first commit

---
+++ InheritanceDemo/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		OgretmenKrediManager ogretmenKrediManager= new OgretmenKrediManager();
+		KrediUI krediUI = new KrediUI();
+		krediUI.KrediHesapla(ogretmenKrediManager);
+
+	}
+
+}
---
+++ interfaces/src/interfaces/MysqlCustomerDal.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public class MysqlCustomerDal implements ICustomerDal, IRepostory{
+
+	@Override
+	public void add() {	
+	System.out.println("My sql eklendi");	
+		
+	}
+
+}
